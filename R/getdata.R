928c1415b13ef8b5a4b165447d4a6f316ce02bd5
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2014-10-22 14:55:53 +0100

    read index and data exe file

---
+++ R/getdata.R
@@ -0,0 +1,29 @@
+getLatestList = function(
+    url="http://www.hscic.gov.uk/searchcatalogue?q=title%3a%22presentation+level+data%22&sort=Most+recent&size=100&page=1"
+    ){
+
+    prescs = rvest::html(url)
+    latest = prescs %>% rvest::html_nodes(".HSCICProducts a") %>% rvest::html_attr("href")
+    latest
+    
+}
+
+
+getExeFromPage <- function(pageurl, dir, overwrite=FALSE){
+    page = rvest::html(pageurl)
+    links = page %>% rvest::html_nodes("#resources a") %>% rvest::html_attr("href")
+    which_exe = grepl(".exe$", links, ignore.case=TRUE)
+    if(!any(which_exe)){
+        stop("No .exe file found in resources links on page")
+    }
+    if(sum(which_exe)>1){
+        stop("More than one exe file found in resources links on page")
+    }
+    get_this = links[which_exe]
+    filename = sub(".*/","", get_this)
+    destfile = file.path(dir, filename)
+    if(file.exists(destfile) & !overwrite){
+        stop("File ",destfile," exists and overwrite=FALSE")
+    }
+    download.file(get_this, destfile)
+}
